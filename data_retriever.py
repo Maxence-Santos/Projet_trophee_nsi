697686f55541183de633e71c2d8091f82843e5b0
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2022-02-16 10:45:30 +0100

    add weather report with api and news scrapper

---
+++ data_retriever.py
@@ -0,0 +1,64 @@
+import json
+from urllib.request import urlopen
+from deep_translator import GoogleTranslator
+from bs4 import BeautifulSoup as bs
+import requests
+
+def weather():
+    with open("conf/ville.txt", "r") as f:
+        ville = f.read()
+    with open("conf/api_key.txt", "r") as f:
+        api_key = f.read()
+    url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={ville}&days=2&aqi=no&alerts=no"
+    reponse = urlopen(url)
+    data = json.loads(reponse.read())["forecast"]["forecastday"]
+    today_weather = {"temp_min":int(data[0]["day"]["mintemp_c"]),
+                     "temp_max": int(data[0]["day"]["maxtemp_c"]),
+                     "vent":int(data[0]["day"]["maxwind_kph"]),
+                     "description":GoogleTranslator(source="english",
+                                                    target="fr").translate(
+                                                            data[0]["day"]["condition"]["text"]
+                                                            )
+                     }
+    tomorrow_weather = {"temp_min": int(data[1]["day"]["mintemp_c"]),
+                     "temp_max": int(data[1]["day"]["maxtemp_c"]),
+                     "vent": int(data[1]["day"]["maxwind_kph"]),
+                     "description": GoogleTranslator(source="english",
+                                                     target="fr").translate(
+                         data[1]["day"]["condition"]["text"]
+                     )
+                     }
+    rapport = f"""
+Aujourd'hui, les températures iront de {today_weather['temp_min']} à {today_weather['temp_max']} degrés.
+Le vent soufflera jusqu'à {today_weather['vent']} km/h. La tendance de la journée est : {today_weather['description']}.
+Demain, les températures iront de {tomorrow_weather['temp_min']} à {tomorrow_weather['temp_max']} degrés.
+Le vent soufflera jusqu'à {tomorrow_weather['vent']} km/h. La tendance de la journée sera : {tomorrow_weather['description']}.
+"""
+    return rapport
+
+def actus():
+    with open("conf/topics.json", "r") as f:
+        topics = json.loads(f.read())["topics"]
+    url = "https://www.20minutes.fr/"
+    articles = []
+    for topic in topics:
+        topic_url = url + topic
+        reponse = requests.get(topic_url)
+        data = bs(reponse.content, 'html.parser')
+        headlines = data.find_all("div", class_="teaser-headline")
+        titres = data.find_all("h2", class_="teaser-title")
+        summary = data.find_all("p", class_="teaser-summary")
+        articles += [article for article in zip(headlines, titres, summary)]
+    articles = list(set(articles))
+    rapport = ""
+    for article in articles:
+        headline = article[0].get_text()
+        titre = article[1].get_text()
+        resume = article[2].get_text()
+        rapport += f"{headline} : {titre}. {resume}.\n"
+    return rapport
+
+if __name__ == "__main__":
+    print(actus())
+
+
