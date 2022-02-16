98cc9e0ca8238223de5c641396ac9b4b67eedc42
Max1433 <maxence1405@free.fr>
2022-02-16 14:56:27 +0100

    modified programm

--- Projet_nsi.ino
+++ Projet_nsi.ino
@@ -2,16 +2,17 @@ int pirPin = 7; // Input for HC-S501
 
 int pirValue; // Place to store read PIR Value
 
-bool flag = false;
+bool flag;
 
 void setup() {
   pinMode(pirPin, INPUT);
   Serial.begin(9600);
   Serial.setTimeout(1);
+  flag = false;
 }
 
 void loop() {
-  /*pirValue = digitalRead(pirPin);
+  pirValue = digitalRead(pirPin);
   if (!pirValue) {
     flag = false;
   }
@@ -21,8 +22,5 @@ void loop() {
       flag = true;
     }
     
-  }*/
-  Serial.println("1");
-  delay(1000);
-  Serial.flush();
+  }
 }
