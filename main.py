15567c0b6f60ddfba513a03ccf89dd1668cbe96b
Max1433 <maxence1405@free.fr>
2022-02-17 15:39:20 +0100

    doesn't open code.exe every time

--- main.py
+++ main.py
@@ -16,21 +16,25 @@ def speak(audio):
     engine.runAndWait()
 
 def trouverInstance():
-    name = 'voice_commands.exe'
+    name1 = 'voice_commands.exe'
+    name2 = "code.exe"
     f = wmi.WMI() 
+    pr = []
     for process in f.Win32_Process(): 
-        if process.name == name: 
+        if process.name == name1: 
             process.Terminate()
+        pr.append(process.name)
+    if "Code.exe" not in pr:
+        os.startfile("C:\\Program Files\\Microsoft VS Code\\Code.exe")
 
 ard = serial.Serial('COM3',timeout=1)
 print(ard)
 
 while True:
     #print(type(str(ard.readline().decode('UTF-8'))))
-    trouverInstance()
     if str(ard.readline().decode('UTF-8')) != "" : 
-        os.startfile("C:\\Program Files\\Microsoft VS Code\\Code.exe")
+        trouverInstance()
         speak(data_retriever.weather())
         speak(data_retriever.actus())
         time.sleep(2)
-        os.startfile("build\\exe.win-amd64-3.8\\voice_commands.exe") 
+        os.startfile("build\\exe.win-amd64-3.8\\voice_commands.exe")
