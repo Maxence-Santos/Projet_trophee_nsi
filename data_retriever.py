48d18855d24bb89561d7823be747565dc1b1ac49
Brice-Vergnou <86613710+Brice-Vergnou@users.noreply.github.com>
2022-02-22 09:58:20 +0100

    Comment code + added issues

--- data_retriever.py
+++ data_retriever.py
@@ -1,3 +1,4 @@
+##### IMPORT DES LIBRAIRIES #####
 import json
 from urllib.request import urlopen
 from deep_translator import GoogleTranslator
@@ -5,14 +6,23 @@ from bs4 import BeautifulSoup as bs
 import requests
 
 
-def weather():
+
+def weather()-> str:
+    '''
+    Fait un rapport météo succinct de la journée et du lendemain
+    Paramètres:
+        - None
+    Retour :
+        - str : Le rapport météo
+    '''
     with open("conf/ville.txt", "r") as f:
-        ville = f.read()
+        ville = f.read() # Note la ville où le rapport météo devra être fait
     with open("conf/api_key.txt", "r") as f:
-        api_key = f.read()
+        api_key = f.read() # Note la clé API de l'utilisateur
     url = f"http://api.weatherapi.com/v1/forecast.json?key={api_key}&q={ville}&days=2&aqi=no&alerts=no"
-    reponse = urlopen(url)
+    reponse = urlopen(url) # Ouvre la page du rapport météo
     data = json.loads(reponse.read())["forecast"]["forecastday"]
+    # Crée un dictionnaire des informations qui seront utiles au rapport ( températures, vent, description )
     today_weather = {"temp_min": int(data[0]["day"]["mintemp_c"]),
                      "temp_max": int(data[0]["day"]["maxtemp_c"]),
                      "vent": int(data[0]["day"]["maxwind_kph"]),
@@ -21,6 +31,7 @@ def weather():
                          data[0]["day"]["condition"]["text"]
                      )
                      }
+    # Fait de même avec les conditions du lendemain
     tomorrow_weather = {"temp_min": int(data[1]["day"]["mintemp_c"]),
                         "temp_max": int(data[1]["day"]["maxtemp_c"]),
                         "vent": int(data[1]["day"]["maxwind_kph"]),
@@ -29,6 +40,7 @@ def weather():
                             data[1]["day"]["condition"]["text"]
                         )
                         }
+    # Reformate les données pour en fait un rapport
     rapport = f"""
 Aujourd'hui, les températures iront de {today_weather['temp_min']} à {today_weather['temp_max']} degrés.
 Le vent soufflera jusqu'à {today_weather['vent']} km/h. La tendance de la journée est : {today_weather['description']}.
@@ -38,7 +50,15 @@ Le vent soufflera jusqu'à {tomorrow_weather['vent']} km/h. La tendance de la jo
     return rapport
 
 
-def actus():
+def actus()-> str:
+    '''
+        Fait un rapport des actualités succinct
+        Paramètres:
+            - None
+        Retour :
+            - str : Le rapport des actualités
+        '''
+    # Ouvre le fichier de configuration pour noter les sujets d'intérêts de l'utilisateur et la qté d'aticles à donner
     with open("conf/topics.json", "r") as f:
         file = json.loads(f.read())
         topics = file["topics"]
@@ -48,14 +68,20 @@ def actus():
     for topic in topics:
         topic_url = url + topic
         reponse = requests.get(topic_url)
+        # Passe le résultats de la requête dans BS pour pouvoir manipuler les éléments html
         data = bs(reponse.content, 'html.parser')
+        # Récupère les titres et résumé en fonction des classes trouvées en inspectant le code source du site
         headlines = data.find_all("div", class_="teaser-headline")
         titres = data.find_all("h2", class_="teaser-title")
         summary = data.find_all("p", class_="teaser-summary")
+        # Crée une liste de tuples mettant ensemble les titres et résumés
         article_topic = [article for article in zip(headlines, titres, summary)]
+        # Enlève les articles en trop en fonction du choix de l'utilisateur
         articles += article_topic[:nb_articles]
+    # Enlève les doublons
     articles = list(set(articles))
     rapport = ""
+    # Récupère le texte de chaque élément html et les formatte pour créer un rapport
     for article in articles:
         headline = article[0].get_text()
         titre = article[1].get_text()
@@ -64,5 +90,8 @@ def actus():
     return rapport
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": # Sert seulement pour tester les fonctions
+    print(weather())
     print(actus())
+
+# TODO : Gérer les erreurs
--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+##### IMPORT DES LIBRAIRIES ######
+
 import serial
 import os
 import pyttsx3
 import data_retriever
 import time
-import psutil
-import subprocess
 import wmi
+
+###### -  ######
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
+ard = serial.Serial('COM3', timeout=1)
 
+##### PROCEDURES PRATIQUES ######
 
-def speak(audio):
+def speak(audio:str)-> None:
+    '''
+    Prend une chaîne de caractères en paramètre et la dit à haute voix
+    Paramètres:
+        - audio : Le chaîne à dire
+    Retour :
+        None
+    '''
     engine.say(audio)
     engine.runAndWait()
 
 
-def trouverInstance():
+def trouverInstance(None)-> None:
+    '''
+    -
+    '''
     f = wmi.WMI()
     pr = []
     for process in f.Win32_Process():
@@ -25,23 +39,23 @@ def trouverInstance():
     if "Code.exe" not in pr:
         os.startfile("C:\\Program Files\\Microsoft VS Code\\Code.exe")
 
+##### PROGRAMME PRINCIPAL ######
 
-ard = serial.Serial('COM3', timeout=1)
-print(ard)
+if __name__ == "__main__":
+    while True:
+        if ard.readline() == b'D\xc3\xa9tect\xc3\xa9\r\n':
+            trouverInstance()
+            speak(data_retriever.weather())
+            speak(data_retriever.actus())
+            time.sleep(2)
+            os.startfile("build\\exe.win-amd64-3.8\\voice_commands.exe")
+        elif ard.readline() == b'Low\r\n':
+            name1 = 'voice_commands.exe'
+            f = wmi.WMI()
+            pr = []
+            for process in f.Win32_Process():
+                if process.name == name1:
+                    process.Terminate()
 
-while True:
-    # print(ard.readline())
-    # if str(ard.readline().decode('UTF-8')) != "" :
-    if ard.readline() == b'D\xc3\xa9tect\xc3\xa9\r\n':
-        trouverInstance()
-        speak(data_retriever.weather())
-        speak(data_retriever.actus())
-        time.sleep(2)
-        os.startfile("build\\exe.win-amd64-3.8\\voice_commands.exe")
-    elif ard.readline() == b'Low\r\n':
-        name1 = 'voice_commands.exe'
-        f = wmi.WMI()
-        pr = []
-        for process in f.Win32_Process():
-            if process.name == name1:
-                process.Terminate()
+# TODO : Commente ton code pour qu'il soit compréhensible
+# TODO : Termine les docstrings
